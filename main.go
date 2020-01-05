0af9b1e68e9dac316b272cdeeb339ce934345a88
orkunkl <kulceorkun@gmail.com>
2020-01-05 21:05:45 +0300

    Add cloudwatch event

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 "context"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 
-"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context) (string, error) {
-	return "hello", nil
+func handler(ctx context.Context, event events.CloudWatchEvent) {
+	fmt.Printf("Detail = %s\n", event.Detail)
 }
 
 func main() {
